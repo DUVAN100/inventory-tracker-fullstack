660f90f1b234869bbd375a990bc36d6e3d6a7b25
DUVAN100 <duvanvivas100@gmail.com>
2025-05-15 18:50:49 -0500

    creating branch

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/products', require('./routes/productRoutes'));
-
+ 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) }); 
